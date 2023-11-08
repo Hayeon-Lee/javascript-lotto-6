27297479189d8b0bd84db06e3f4fbbd287892aa4
Hayeon-Lee <papepopepe@naver.com>
2023-11-08 16:38:38 +0900

    feat: App.js 에서 Input 클래스를 선언하고 필요한 필드를 만듭니다.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
+import Input from './view/Input.js';
+
 class App {
+  #money;
+  #lotto;
+  #winningNumbers;
+  #bonusNumber;
+
+  constructor() {
+    this.#money = 0;
+    this.#lotto = [];
+    this.#winningNumbers = [];
+    this.#bonusNumber = 0;
+    this.input = new Input();
+  }
+
   async play() {}
 }
+
+const app = new App();
+app.play();
 
 export default App;
