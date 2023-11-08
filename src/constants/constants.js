4e9bc85a1c337ab59276a47d822bec315051903d
Hayeon-Lee <papepopepe@naver.com>
2023-11-08 16:09:29 +0900

    feat: 입력 메세지와 출력 메세지를 상수 파일에 선언합니다.

---
+++ src/constants/constants.js
@@ -0,0 +1,11 @@
+const USER_INPUT_MESSAGE = Object.freeze({
+  inputMoneyMessage: '구입금액을 입력해 주세요.\n',
+  inputWinningNumbersMessage: '당첨번호를 입력해 주세요.\n',
+  inputBonusNumberMessage: '보너스 번호를 입력해 주세요.\n',
+});
+
+const NOTICE_MESSAGE = Object.freeze({
+  noticeTicketsAmount: '개를 구매했습니다.\n',
+  noticeWinningAnalysis: '당첨 통계\n---\n',
+  noticeUserProfit: '총 수익률은 *%입니다.',
+});
